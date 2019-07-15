abdaf1bf1e4f746966af7d80c1d0232c3d52d0ee
Michael Yankura <myankura210@gmail.com>
2019-07-15 11:27:28 -0500

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First/Middle Name")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name ="Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
